91778ee42cf8731ef2ee380b130c5af27aedbad2
David Molinero <doktor500@users.noreply.github.com>
2024-12-04 21:31:22 +0000

    Refactors

--- src/main/kotlin/uk/co/kenfos/ChallengeDay4.kt
+++ src/main/kotlin/uk/co/kenfos/ChallengeDay4.kt
@@ -1,16 +1,20 @@
 package uk.co.kenfos
 
 fun findCombinations(input: String, word: String): Int {
-    val grid = Grid(input, word.length - 1)
+    val grid = Grid(input, searchRange = word.length - 1)
     val firstCharacter = word.first()
 
     fun countCombinations(rowIndex: Int, columnIndex: Int): Int {
-        return grid.getCombinations(rowIndex, columnIndex).map { it.joinToString("") }.count { it == word }
+        return grid
+            .getCombinations(rowIndex, columnIndex)
+            .map { it.joinToString("") }
+            .count { it == word }
     }
 
     fun getRowMatches(row: CharArray, rowIndex: Int): List<Int> {
         return row.mapIndexed { columnIndex, character ->
-            if (character == firstCharacter) countCombinations(rowIndex, columnIndex) else 0
+            val validMatch = character == firstCharacter
+            if (validMatch) countCombinations(rowIndex, columnIndex) else 0
         }
     }
 
@@ -18,20 +22,23 @@ fun findCombinations(input: String, word: String): Int {
 }
 
 fun findXCombinations(input: String, word: String): Int {
-    val grid = Grid(input, word.length - 1)
+    val grid = Grid(input, searchRange = word.length - 1)
     val firstCharacter = word.first()
     val lastCharacter = word.last()
     val xCombinationCount = 2
 
-    fun countValidCombinations(rowIndex: Int, columnIndex: Int): Int {
-        val combinations = grid.getXCombinations(rowIndex, columnIndex).map { it.joinToString("") }.count { it == word }
-        return if (combinations == xCombinationCount) 1 else 0
+    fun countCombinations(rowIndex: Int, columnIndex: Int): Int {
+        return grid
+            .getXCombinations(rowIndex, columnIndex)
+            .map { it.joinToString("") }
+            .count { it == word }
+            .let { if (it == xCombinationCount) 1 else 0 }
     }
 
     fun getRowMatches(row: CharArray, rowIndex: Int): List<Int> {
         return row.mapIndexed { columnIndex, character ->
-            val characterMatch = character == firstCharacter || character == lastCharacter
-            if (characterMatch) countValidCombinations(rowIndex, columnIndex) else 0
+            val validMatch = character == firstCharacter || character == lastCharacter
+            if (validMatch) countCombinations(rowIndex, columnIndex) else 0
         }
     }
 
@@ -41,57 +48,34 @@ fun findXCombinations(input: String, word: String): Int {
 private data class Grid(val rows: List<CharArray>, val searchRange: Int) {
     constructor(input: String, searchRange: Int) : this(input.lines().map { it.toCharArray() }, searchRange)
 
-    fun getCombinations(row: Int, column: Int): List<List<Char>> {
-        return listOf(
-            getHorizontal(row, column),
-            getBackwardHorizontal(row, column),
-            getVertical(row, column),
-            getBackwardVertical(row, column),
-            getRightDiagonal(row, column),
-            getBackwardRightDiagonal(row, column),
-            getLeftDiagonal(row, column),
-            getBackwardLeftDiagonal(row, column)
-        )
-    }
+    fun getCombinations(row: Int, column: Int) = listOf(
+        getEastLine(row, column),
+        getWestLine(row, column),
+        getSouthLine(row, column),
+        getNorthLine(row, column),
+        getSouthEastLine(row, column),
+        getSouthWestLine(row, column),
+        getNorthEastLine(row, column),
+        getNorthWestLine(row, column)
+    )
 
-    fun getXCombinations(row: Int, column: Int): List<List<Char>> {
-        return listOf(
-            getRightDiagonal(row, column),
-            getBackwardRightDiagonal(row, column + searchRange).reversed(),
-            getLeftDiagonal(row + searchRange, column).reversed(),
-            getBackwardLeftDiagonal(row + searchRange, column + searchRange)
-        )
-    }
+    fun getXCombinations(row: Int, column: Int) = listOf(
+        getSouthEastLine(row, column),
+        getNorthEastLine(row + searchRange, column),
+        getSouthWestLine(row, column + searchRange),
+        getNorthWestLine(row + searchRange, column + searchRange)
+    )
 
-    private fun getHorizontal(row: Int, column: Int): List<Char> {
-        return (0..searchRange).mapNotNull { rows[row].getOrNull(column + it) }
-    }
+    private fun getEastLine(row: Int, column: Int) = getLine { row to column + it }
+    private fun getWestLine(row: Int, column: Int) = getLine { row to column - it }
+    private fun getSouthLine(row: Int, column: Int) = getLine { row + it to column }
+    private fun getNorthLine(row: Int, column: Int) = getLine { row - it to column }
+    private fun getSouthEastLine(row: Int, column: Int) = getLine { row + it to column + it }
+    private fun getSouthWestLine(row: Int, column: Int) = getLine { row + it to column - it }
+    private fun getNorthEastLine(row: Int, column: Int) = getLine { row - it to column + it }
+    private fun getNorthWestLine(row: Int, column: Int) = getLine { row - it to column - it }
 
-    private fun getBackwardHorizontal(row: Int, column: Int): List<Char> {
-        return (0..searchRange).mapNotNull { rows[row].getOrNull(column - it) }
-    }
-
-    private fun getVertical(row: Int, column: Int): List<Char> {
-        return (0..searchRange).mapNotNull { rows.getOrNull(row + it)?.get(column) }
-    }
-
-    private fun getBackwardVertical(row: Int, column: Int): List<Char> {
-        return (0..searchRange).mapNotNull { rows.getOrNull(row - it)?.get(column) }
-    }
-
-    private fun getRightDiagonal(row: Int, column: Int): List<Char> {
-        return (0..searchRange).mapNotNull { rows.getOrNull(row + it)?.getOrNull(column + it) }
-    }
-
-    private fun getBackwardRightDiagonal(row: Int, column: Int): List<Char> {
-        return (0..searchRange).mapNotNull { rows.getOrNull(row + it)?.getOrNull(column - it) }
-    }
-
-    private fun getLeftDiagonal(row: Int, column: Int): List<Char> {
-        return (0..searchRange).mapNotNull { rows.getOrNull(row - it)?.getOrNull(column + it) }
-    }
-
-    private fun getBackwardLeftDiagonal(row: Int, column: Int): List<Char> {
-        return (0..searchRange).mapNotNull { rows.getOrNull(row - it)?.getOrNull(column - it) }
+    private fun getLine(line: (index: Int) -> Pair<Int, Int>): List<Char> {
+        return (0..searchRange).mapNotNull { line(it).let { (row, column) -> rows.getOrNull(row)?.getOrNull(column) } }
     }
 }
